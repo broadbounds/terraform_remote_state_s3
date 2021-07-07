2ab4e9570b48416f3e58e479b23ca43d1d55b290
broadbounds <84296908+broadbounds@users.noreply.github.com>
2021-07-07 09:57:35 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -67,7 +67,19 @@ resource "aws_dynamodb_table" "terraform_locks" {
 # CONFIGURE TERRAFORM BACKEND
 # ------------------------------------------------------------------------------
 
-
+terraform {
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket         = "broadbounds-terraform-remote-state-s3"
+    key            = "global/s3/terraform.tfstate"
+    region         = var.aws_region
+    access_key = var.access_key
+    secret_key = var.secret_key
+    # Replace this with your DynamoDB table name!
+    dynamodb_table = "broadbounds-terraform-remote-state-s3-locks"
+    encrypt        = true
+  }
+}
 
 
 # We create a new VPC
